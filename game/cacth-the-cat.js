a34b3deb9006799215dc63a27c564428e0329370
duckbil <2761663853@qq.com>
2023-12-17 15:13:00 +0800

    Site updated: 2023-12-17 15:12:59

--- game/cacth-the-cat.js
+++ game/cacth-the-cat.js
@@ -1,1 +1,14 @@
 <script src="catch-the-cat.js"></script>
+<div id="catch-the-cat"></div>
+<script>
+    window.game = new CatchTheCatGame({
+        w: 11,
+        h: 11,
+        r: 20,
+        initialWallCount: 8,
+        backgroundColor: 0xeeeeee,
+        parent: 'catch-the-cat',
+        statusBarAlign: 'center',
+        credit: 'github.com/ganlvtech'
+    });
+</script>
